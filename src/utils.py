58db5e86a86926017649eb2b64fe7a8458af7e25
Oscar <oscarodriguez56@gmail.com>
2025-01-12 13:21:33 +0100

    Cambio de nombre

--- src/utils.py
+++ src/utils.py
@@ -19,5 +19,5 @@ def generar_codigo_genero(nomg) -> str:
     return nomg[0:2].upper()  # Devolvemos las dos primeras letras del genero en mayusculas
 
 if __name__ == '__main__':
-    lista = [["a5"],["a1"],["a10"],["a3"],["a4"]]
-    print(generar_numero_nuevo_codigo(lista,"a"))
+    lis = [["a5"], ["a1"], ["a10"], ["a3"], ["a4"]]
+    print(generar_numero_nuevo_codigo(lis, "a"))
