4b9c2ef2e9deee0509364732c504392fdb854174
Oscar <oscarodriguez56@gmail.com>
2025-01-12 12:49:00 +0100

    Adaptando la funcion generar_numero_nuevo_codigo a la manera en la que la base de datos devuelve el codigo

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,11 @@
 def generar_numero_nuevo_codigo(lista, letra):
     numero_codigo = 1
-    for list in lista:
-        numero = int(list.replace(letra,""))
+    for elemento in lista:
+        numero = int(elemento[0].replace(letra,""))
         if numero>numero_codigo:
             numero_codigo = numero
     return numero_codigo
 
 if __name__ == '__main__':
-    lista = ["a5","a1","a10","a3","a4"]
+    lista = [["a5"],["a1"],["a10"],["a3"],["a4"]]
     print(generar_numero_nuevo_codigo(lista,"a"))
