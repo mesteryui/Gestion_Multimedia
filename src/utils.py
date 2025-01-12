8df5b1f57884a85736660eb2c079ff4cb452d765
Oscar <oscarodriguez56@gmail.com>
2025-01-12 12:52:38 +0100

    Haciendo que la funcion generar numero genere el numero +1 que es el numero que se utilizara en el codigo nuevo

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ def generar_numero_nuevo_codigo(lista, letra):
         numero = int(elemento[0].replace(letra,""))
         if numero>numero_codigo:
             numero_codigo = numero
-    return numero_codigo
+    return numero_codigo+1
 
 if __name__ == '__main__':
     lista = [["a5"],["a1"],["a10"],["a3"],["a4"]]
